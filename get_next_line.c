0c4cdf55cffc6c55aed14d7d3cf71810fe1b6ac3
namejojo <jlima-so@42lisboa.com>
2025-04-09 17:46:06 +0100

    msg

--- get_next_line.c
+++ get_next_line.c
@@ -6,12 +6,12 @@
 /*   By: jlima-so <jlima-so@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 16:13:13 by jlima-so          #+#    #+#             */
-/*   Updated: 2025/04/08 20:34:02 by jlima-so         ###   ########.fr       */
+/*   Updated: 2025/04/09 17:38:17 by jlima-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
+#include <stdio.h>
 
 
 
@@ -22,15 +22,15 @@ char	*get_next_line(int fd)
 	char		*ret;
 	int			i;
 
+	if (read(fd, &i, 0) == -1)
+		return (NULL);
 	i = 1;
 	if (*keep == '\0')
 	{
 		ft_bzero(keep, BUFFER_SIZE + 1);
 		i = read (fd, keep, BUFFER_SIZE);
-		if (i < 1 && (*keep == '\0' || !check(keep)))
-			return (NULL);
 	}
-	if (i < 1 && *keep == '\0')
+	if (i < 1 && (*keep == '\0' || !check(keep)))
 		return (NULL);
 	ret = ft_strjoin(NULL, keep);
 	while (i && check(ret))
--- main.c
+++ main.c
@@ -3,75 +3,40 @@
 
 int main(void)
 {
-	int		i;
-	char	*ret;
-	// char	*a;
+        int             i;
+        char    *ret;
 
-	// a = "\n\n\nola";a
-	// char *ret="ola bom dia \n1olla ola\n";
-	// char ret1[50]="eu vou dizer/";
-	//
-	i = open("teste.txt", O_RDWR, 00700);
-	// 
-	ret = get_next_line(i);
-	printf("%s", ret);
-	if (ret != NULL)
+        i = open("teste.txt", O_RDWR);
+        // do {
+                // ret = get_next_line(i);
+                // printf("%s", ret);
+                // if (ret != NULL)
+                        // free (ret);
+        // } while (ret != NULL);
+		ret = get_next_line(i);
+		printf("%s", ret);
+		if (ret != NULL)
+			free (ret);
+// 
+        ret = get_next_line(i);
+        printf("%s", ret);
+        if (ret != NULL)
+            free (ret);
+// 
+		ret = get_next_line(i);
+		printf("%s", ret);
+		if (ret != NULL)
 		free (ret);
-	ret = get_next_line(i);
-	printf("%s", ret);
-	if (ret != NULL)
-		free (ret);
-	// ret = get_next_line(i);
-	// printf("%s", ret);
-	// if (ret != NULL)
-	// 	free (ret);
-	// ret = get_next_line(i);
-	// printf("%s", ret);
-	// if (ret != NULL)
-	// 	free (ret);
-	// ret = get_next_line(i);
-	// printf("%s", ret);
-	// if (ret != NULL)
-	// 	free (ret);
-	// ret = get_next_line(i);
-	// printf("%s", ret);
-	// if (ret != NULL)
-	// 	free (ret);
-	// ret = get_next_line(i);
-	// printf("%s", ret);
-	// if (ret != NULL)
-	// 	free (ret);
-	// ret = get_next_line(i);
-	// printf("%s", ret);
-	// if (ret != NULL)
-	// 	free (ret);
-	// ret = get_next_line(i);
-	// printf("%s", ret);
-	// if (ret != NULL)
-	// 	free (ret);
-	// ret = get_next_line(i);
-	// printf("%s", ret);
-	// if (ret != NULL)
-	// 	free (ret);
-	// ret = get_next_line(i);
-	// printf("%s", ret);
-	// if (ret != NULL)
-	// 	free (ret);
-		
+// 
+		ret = get_next_line(i);
+		printf("%s", ret);
+		if (ret != NULL)
+				free (ret);
+// 
+	printf("this fd is wrong\n");
 
-	// ret = get_next_line(i);
-	// printf("%s", ret);
-	// if (ret != NULL)
-	// 	free (ret);
-
-	// ret = get_next_line(i);
-	// printf("%s", ret);
-	// if (ret != NULL)
-	// 	free (ret);
-
-	// ret = get_next_line(i);
-	// printf("%s", ret);
-	// if (ret != NULL)
-	// 	free (ret);
-
+		ret = get_next_line(10000);
+		printf("%s", ret);
+		if (ret != NULL)
+				free (ret);
 }
